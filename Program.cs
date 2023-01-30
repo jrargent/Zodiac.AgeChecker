da73e6712f312ac4041a45b779b761c6a03dc876
Argent <jrargent42@gmail.com>
2023-01-30 13:14:06 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿//Goal of program is to have User input their birthday and have console return User's current age and their Zodiac sign
+
+using Zodiac.AgeChecker;
+
+Console.WriteLine("Please enter your Birth Month: ");
+
+string month = Console.ReadLine() ?? ""; // will need to parse to int? ex. Jan = 01?
+
+
+Console.WriteLine("Please enter your Birth Day: ");
+
+string day = Console.ReadLine() ?? ""; // will need to parse to int,but what do if user types in day that doesn't exist, like 70?
+
+
+Console.WriteLine("Please enter your Birth Year: ");
+
+string year = Console.ReadLine() ?? "";
+
+DateTime birthday = new DateTime(int.Parse(year), int.Parse(month), int.Parse(day));
+
+ZodiacModel userZodiac = new ZodiacModel().CheckZodiac(birthday);
+
+
+string today = DateTime.Now.ToString("yyyyMMdd");
+string birth = birthday.ToString("yyyyMMdd");
+
+
+int age = int.Parse(today) - int.Parse(birth);
+Console.WriteLine("You are " + string.Concat(age.ToString()[0], age.ToString()[1]) + " years old!");
+
+Console.WriteLine("Your zodiac sign is " + userZodiac.Name + "!");
+
+//Console.WriteLine(birthday.ToString());
+
---
+++ ZodiacModel.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zodiac.AgeChecker
+{
+    public class ZodiacModel // properties are: name, date from, date to
+    {
+        public string Name { get; set; }
+        public DateTime DateFrom { get; set; }
+        public DateTime DateTo { get; set; }
+
+        public ZodiacModel CheckZodiac(DateTime birthday) 
+        {
+            // if entered datetime is X, then zodiac sign is Y
+            // have for loop to go through zodiac list, when datetime matches, break loop and return zodiac sign from list
+
+            ZodiacModel ret = null;
+
+            List<ZodiacModel> zodiacs = GetZodiacs(birthday.Year);
+            foreach (ZodiacModel zodiac in zodiacs) 
+            {
+                if (birthday >= zodiac.DateFrom && birthday <= zodiac.DateTo)
+                {
+                    ret = zodiac; // this is what will use console.WriteLine(zodiac.name) in Main
+                }
+            }
+            return ret;
+
+        }
+
+        private List<ZodiacModel> GetZodiacs(int birthyear) // will become GetZodiacs(int birthyear). Will then get rid of birthdate. all spots of birthdate.year will update to birthyear
+        {
+            
+
+
+            return new List<ZodiacModel>()
+            {
+                new ZodiacModel()
+                {
+                    DateFrom = new DateTime(birthyear, 01, 01),
+                    DateTo = new DateTime(birthyear, 01, 20),
+                    Name = "Capricorn"
+                },
+                new ZodiacModel() // creating an object with these values and then including in the Zodiac list
+                {
+                    DateFrom = new DateTime(birthyear, 01, 21),
+                    DateTo = new DateTime(birthyear, 02, 19),
+                    Name = "Aquarius"
+                },
+                new ZodiacModel()
+                {
+                    DateFrom = new DateTime(birthyear, 02, 20),
+                    DateTo = new DateTime(birthyear, 03, 20),
+                    Name = "Pisces"
+                },
+                new ZodiacModel()
+                {
+                    DateFrom = new DateTime(birthyear, 03, 21),
+                    DateTo = new DateTime(birthyear, 04, 20),
+                    Name = "Aries"
+                },
+                new ZodiacModel()
+                {
+                    DateFrom = new DateTime(birthyear, 04, 21),
+                    DateTo = new DateTime(birthyear, 05, 20),
+                    Name = "Taurus"
+                },
+                new ZodiacModel()
+                {
+                    DateFrom = new DateTime(birthyear, 05, 21),
+                    DateTo = new DateTime(birthyear, 06, 20),
+                    Name = "Gemini"
+                },
+                new ZodiacModel()
+                {
+                    DateFrom = new DateTime(birthyear, 06, 21),
+                    DateTo = new DateTime(birthyear, 07, 21),
+                    Name = "Cancer"
+                },
+                new ZodiacModel()
+                {
+                    DateFrom = new DateTime(birthyear, 07, 22),
+                    DateTo = new DateTime(birthyear, 08, 22),
+                    Name = "Leo"
+                },
+                new ZodiacModel()
+                {
+                    DateFrom = new DateTime(birthyear, 08, 23),
+                    DateTo = new DateTime(birthyear, 09, 21),
+                    Name = "Virgo"
+                },
+                new ZodiacModel()
+                {
+                    DateFrom = new DateTime(birthyear, 09, 22),
+                    DateTo = new DateTime(birthyear, 10, 21),
+                    Name = "Libra"
+                },
+                new ZodiacModel()
+                {
+                    DateFrom = new DateTime(birthyear, 10, 22),
+                    DateTo = new DateTime(birthyear, 11, 21),
+                    Name = "Scorpio"
+                },
+                new ZodiacModel()
+                {
+                    DateFrom = new DateTime(birthyear, 11, 22),
+                    DateTo = new DateTime(birthyear, 12, 21),
+                    Name = "Sagittarius"
+                },
+                new ZodiacModel()
+                {
+                    DateFrom = new DateTime(birthyear, 12, 22),
+                    DateTo = new DateTime(birthyear, 12, 31), 
+                    Name = "Capricorn"
+                }
+            };
+        }
+    }
+}
